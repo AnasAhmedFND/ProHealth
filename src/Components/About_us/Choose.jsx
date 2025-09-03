e97fd29985a628a632ca8dce3997e9ec009746d8
AnasAhmedFND <anasahmed4402@gmail.com>
2025-09-03 10:27:27 +0600

    choose card update

--- src/Components/About_us/Choose.jsx
+++ src/Components/About_us/Choose.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
-
+"use client"
+import React, { useState } from 'react'
+import { motion } from "framer-motion";
 const Choose = () => {
+    const [count, setCount]=useState(0)
     return (
         <section className='container mx-auto py-10'>
             {/* choose mother div */}
@@ -24,30 +26,30 @@ const Choose = () => {
                                 possible care to our patients.</small> </p>
                         </div>
 
-                         <div className=" w-[49%] py-3 ">
+                        <motion.div
+                            initial={{ opacity: 5 }}
+                            whileInView={{ opacity: 4 }}
+                            className=" w-[49%] py-3 ">
                             <img className='border rounded-full w-[40px] h-[40px] ' src="home/dep2-pic.png" alt="" />
-                            <h4 className='font-bold text-xl mt-2'>Experienced Medical  <br />
-                                Professionals</h4>
+                            <h4 className='font-bold text-xl mt-2'>Comprehensive Services </h4>
+                            <p className='mt-2'><small> Our team includes experienced doctors, <br />
+                                nurses, and other healthcare professionals <br />
+                                who are dedicated to providing the best <br />
+                                possible care to our patients.</small> </p>
+                        </motion.div>
+
+                        <div className=" w-[49%] py-3 ">
+                            <img className='border rounded-full w-[40px] h-[40px] ' src="home/dep2-pic.png" alt="" />
+                            <h4 className='font-bold text-xl mt-2'>Patient-centered Approach  </h4>
                             <p className='mt-2'><small> Our team includes experienced doctors, <br />
                                 nurses, and other healthcare professionals <br />
                                 who are dedicated to providing the best <br />
                                 possible care to our patients.</small> </p>
                         </div>
 
-                         <div className=" w-[49%] py-3 ">
+                        <div className=" w-[49%] py-3 ">
                             <img className='border rounded-full w-[40px] h-[40px] ' src="home/dep2-pic.png" alt="" />
-                            <h4 className='font-bold text-xl mt-2'>Experienced Medical  <br />
-                                Professionals</h4>
-                            <p className='mt-2'><small> Our team includes experienced doctors, <br />
-                                nurses, and other healthcare professionals <br />
-                                who are dedicated to providing the best <br />
-                                possible care to our patients.</small> </p>
-                        </div>
-
-                         <div className=" w-[49%] py-3 ">
-                            <img className='border rounded-full w-[40px] h-[40px] ' src="home/dep2-pic.png" alt="" />
-                            <h4 className='font-bold text-xl mt-2'>Experienced Medical  <br />
-                                Professionals</h4>
+                            <h4 className='font-bold text-xl mt-2'>State-of-the-art Facilities </h4>
                             <p className='mt-2'><small> Our team includes experienced doctors, <br />
                                 nurses, and other healthcare professionals <br />
                                 who are dedicated to providing the best <br />
@@ -58,8 +60,9 @@ const Choose = () => {
 
 
                 </div>
-            </div>
 
+            </div>
+                
         </section>
     )
 }
