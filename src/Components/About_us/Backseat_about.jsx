60fb39deb3aeb02c2eed626f80d1e942409eba64
AnasAhmedFND <anasahmed4402@gmail.com>
2025-09-06 16:10:36 +0600

    about page in recognition section done

---
+++ src/Components/About_us/Backseat_about.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Backseat_about = () => {
+    return (
+        <section className='container mx-auto border py-[250px] text-center text-white relative top-[220px] ' style={{ backgroundImage: "url('/about/fram_bg.png')" }}>
+            
+                <h2 className='font-bold text-4xl'>Don’t Let Your Health <br />
+                    Take a Backseat!</h2>
+                <p className='mt-3'>Schedule an appointment with one of our experienced <br />
+                    medical professionals today!</p>
+            
+
+
+        </section>
+    )
+}
+
+export default Backseat_about
--- src/app/about_us/page.jsx
+++ src/app/about_us/page.jsx
@@ -1,6 +1,7 @@
 import About_services from '@/Components/About_us/About_services'
 import Abouts_us from '@/Components/About_us/Abouts_us'
 import Activities from '@/Components/About_us/Activities'
+import Backseat_about from '@/Components/About_us/Backseat_about'
 import Choose from '@/Components/About_us/Choose'
 import Expert from '@/Components/About_us/Expert'
 import Recognition from '@/Components/About_us/Recognition'
@@ -16,6 +17,7 @@ const page = () => {
       <Expert/>
       <Activities/>
       <Recognition/>
+      <Backseat_about/>
 
     </>
   )
